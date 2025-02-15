9d885820fadc7ab24ef80fb1b7a49e6a7c629ae5
Prads121 <anand.pradyumna1@gmail.com>
2025-02-15 19:23:23 +0530

    Made some changes in the pythoncod file

--- Heart.py
+++ Heart.py
@@ -2,26 +2,23 @@ import turtle as t
 
 t.bgcolor("black")
 t.pensize(5)
-t.speed(3)
+t.speed(1)
 t.color("red")
 t.begin_fill()
 t.fillcolor('red')
-
 t.left(140)
 t.forward(180)
-t.circle(-90, 180)
-t.left(90)
-t.circle(-90, 180)
-t.forward(180)
+t.circle(-90,180)
 t.setheading(50)
+t.circle(-90,180)
+t.forward(180)
 t.end_fill()
-
 t.penup()
-t.goto(200, 0)
+t.goto(100, 0) 
 t.pendown()
-
-t.color("red")
-t.write("Fuck You", font=("Arial", 24, "bold"), align="center")
-
+msg= "Will you be my valentine"
+t.write(msg, move=True,align ="left", font=("Arial", 24, "bold"))
 t.hideturtle()
 t.done()
+print("what")
+
---
+++ pythoncode-original.py
@@ -0,0 +1,8 @@
+for i in range(0,2):
+    print(str(i)+" hello")
+    
+if i == 1:
+    print("\n0 was the first print")
+else:
+    print("\n1 was the first print")
+print("\njust made a change")
